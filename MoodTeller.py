f573b6bbdaba30a9afbd456132bd6fdca97fe593
Svanik-Jain <130584200+Svanik-Jain@users.noreply.github.com>
2023-06-20 22:54:04 +0530

    Update MoodTeller.py

--- MoodTeller.py
+++ MoodTeller.py
@@ -1,27 +1,23 @@
-import nltk
-from nltk.sentiment import SentimentIntensityAnalyzer
-import matplotlib.pyplot as plt
-import numpy as np
 import speech_recognition as sr
-
-sia = SentimentIntensityAnalyzer()
+from textblob import TextBlob
 
 r = sr.Recognizer()                                                                                   
 with sr.Microphone() as source:                                                                       
     print("Listening...")                                                                                   
     audio = r.listen(source)   
-saidText = r.recognize_google(audio)
 try:
+    saidText = r.recognize_google(audio)
     print("You said " + r.recognize_google(audio))
 except sr.UnknownValueError:
     print("Could not understand audio")
-   
-scoreV = sia.polarity_scores(saidText)
-ScoreC = scoreV['compound']
 
-if(ScoreC == 0):
-    print("You are in a neutral mood.")
-elif(ScoreC >= 0.5):
-    print("You are in a good mood. I hope you have a nice day.")
-elif(ScoreC < 0.5):
-    print("You are in a bad mood. I hope your problems are solved.")
+blob = TextBlob(saidText)
+for sentence in blob.sentences:
+    if(sentence.sentiment.polarity == 0):
+        print("You are in a neutral mood.")
+    elif(sentence.sentiment.polarity > 0):
+        print("You are in a good mood. I hope you have a nice day.")
+    elif(sentence.sentiment.polarity < 0):
+        print("You are in a bad mood. I hope your problems are solved.")
+    else:
+        print("Unable to guess")
